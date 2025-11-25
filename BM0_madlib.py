a523961c45510a995454a7fe2cff59e00aa139d9
adamRevans <adame84@nycstudents.net>
2025-11-25 11:54:55 -0500

    Added first lib

--- BM0_madlib.py
+++ BM0_madlib.py
@@ -1,1 +1,27 @@
-print("hellow world")
+print("hello world")
+#name = input("Enter your name:")
+#print(f"Hello {name}")
+#name = input("Enter your name:")
+#print(f"Hello {name}")
+#fav1 = input("What is your favorite animal:")
+#fav2 = input("What is your favorite color:")
+#fav3 = input("What is your favorite number:")
+#print(f"Do you want a {fav2} {fav1} with {fav3} legs?")
+#Press / to see a list of what the words mean
+
+
+
+
+wrd0 = input("Type an adjective: ")
+wrd1 = input("Type a noun: ")
+wrd2 = input("Type a number: ")
+wrd3 = input("Type an adjective: ")
+wrd4 = input("Type a location: ")
+wrd5 = input("Type a noun: ")
+wrd6 = input("Type a past tense verb: ")
+wrd7 = input("Type a number: ")
+wrd8 = input("Type a part of the body: ")
+wrd9 = input("Type a noun: ")
+madlib0 = ("Once upon a time, there was a {wrd0} {wrd1}, who could only say '{wrd2}''. This was due to a/an {wrd3} skin condition given to them in {wrd4}. Alas, their numerous {wrd5} took advantage of this conundrum and {wrd6} them {wrd7} times in the {wrd8} with a/an {wrd9}")
+#Once upon a time, there was a fair maiden, who could only say '67'. This was due to a/an terrible skin condition given to them in Maine. Alas, their numerous enemies took advantage of this conundrum and stabbed them 67 times in the back with a/an knife.
+#print({madlib0})
