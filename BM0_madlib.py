5ba14a17e5eaea9d21b48f7f66a6cb319bdb8b19
adamRevans <adame84@nycstudents.net>
2025-12-02 11:25:12 -0500

    Update BM0_madlib.py

--- BM0_madlib.py
+++ BM0_madlib.py
@@ -1,27 +1,72 @@
+
+#Start
 print("hello world")
-#name = input("Enter your name:")
-#print(f"Hello {name}")
-#name = input("Enter your name:")
-#print(f"Hello {name}")
-#fav1 = input("What is your favorite animal:")
-#fav2 = input("What is your favorite color:")
-#fav3 = input("What is your favorite number:")
-#print(f"Do you want a {fav2} {fav1} with {fav3} legs?")
-#Press / to see a list of what the words mean
+name = input("Enter your name: ")
+print(f"Hello {name}! Madlib time!")
+###______________________________________###
 
+madlibType = [0,1,2]
 
+random_number = random.choice(madlibType)
 
+    if random_number == 0
+        {wrd0} = input("Type an adjective: ")
+        {wrd1} = input("Type a noun: ")
+        {wrd2} = input("Type a number: ")
+        {wrd3} = input("Type an adjective: ")
+        {wrd4} = input("Type a location: ")
+        {wrd5} = input("Type a noun: ")
+        {wrd6} = input("Type a past tense verb: ")
+        {wrd7} = input("Type a number: ")
+        {wrd8} = input("Type a part of the body: ")
+        {wrd9} = input("Type a noun: ")
 
-wrd0 = input("Type an adjective: ")
-wrd1 = input("Type a noun: ")
-wrd2 = input("Type a number: ")
-wrd3 = input("Type an adjective: ")
-wrd4 = input("Type a location: ")
-wrd5 = input("Type a noun: ")
-wrd6 = input("Type a past tense verb: ")
-wrd7 = input("Type a number: ")
-wrd8 = input("Type a part of the body: ")
-wrd9 = input("Type a noun: ")
-madlib0 = ("Once upon a time, there was a {wrd0} {wrd1}, who could only say '{wrd2}''. This was due to a/an {wrd3} skin condition given to them in {wrd4}. Alas, their numerous {wrd5} took advantage of this conundrum and {wrd6} them {wrd7} times in the {wrd8} with a/an {wrd9}")
+        print("Once upon a time, there was a {wrd0} {wrd1}, who could only say '{wrd2}''. This was due to a/an {wrd3} skin condition given to them in {wrd4}. Alas, their numerous {wrd5} took advantage of this conundrum and {wrd6} them {wrd7} times in the {wrd8} with a/an {wrd9}")
+
 #Once upon a time, there was a fair maiden, who could only say '67'. This was due to a/an terrible skin condition given to them in Maine. Alas, their numerous enemies took advantage of this conundrum and stabbed them 67 times in the back with a/an knife.
-#print({madlib0})
+###_________________________________________###
+
+    if random_number == 1
+        {wrd0} = input(“Type a noun: “)
+        {wrd1} = input(“Type a noun: “)
+        {wrd2} = input(“Type a part of the body: “)
+        {wrd3} = input(“Type a terrible place: “)
+        {wrd4} = input(“Type a noun: “)
+        {wrd5} = input(“Type a verb ending in ing: “)
+        {wrd6} = input(“Type a name: “)
+        {wrd7} = input(“Type a past tense verb: “)
+        {wrd8} = input(“Type a plural noun: “)
+
+        print(“The last thing you want in your {wrd0} king {wrd1} is someone’s {wrd2} fungus, but that may be what you get. An anonymous {wrd3} user posted a {wrd4} of themself {wrd5} in a box of {wrd0} king {wrd1}. Then they got fired. I don’t know what else to write here. So uh. {wrd6} {wrd7} {wrd8}.”)
+
+#The last thing you want in your burger king burger is someone's foot fungus, but that may be what you get. An anonymous 4chan user posted a picture of themself standing in a box of burger king lettuce. Then they got fired. 
+###_________________________________________###
+
+    if random_number == 2
+	{wrd0} = input(“Type a name: “)
+	{wrd1} = input(“Type a verb ending in ing: “)
+	{wrd2} = input(“Type a verb: “)
+	{wrd3} = input(“Type an adjective: “)
+	{wrd4} = input(“Type a noun: “)
+	{wrd5} = input(“Type an adjective: “)
+	{wrd6} = input(“Type a name: “)
+	{wrd7} = input(“Type a verb: “)
+	{wrd8} = input(“Type an adjective: “)
+	{wrd9} = input(“Type an adjective: “)
+	{wrd10} = input(“Type a part of the body: “)
+
+        print(“Oh, there once was a hero named {wrd0} the Red who came {wrd1} to Whiterun from ole Rorikstead. And the braggart did swagger and {wrd2} his blade as he told of {wrd3} battles and {wrd4} he had made. But then he went {wrd5}, did {wrd0} the Red when he met the Shielmaiden {wrd6} who said: Oh you talk and you {wrd7} and you drink all our mead now I think it’s high time that you lie down and bleed. And so then came the clashing and slashing of steel as the brave lass {wrd6} charged in full of zeal. And the braggart named {wrd0} was boastful no more, when his {wrd8} {wrd9} {wrd10} rolled around on the floor.”
+
+#Oh, there once was a hero named Ragnar the Red
+#Who came riding to Whiterun from ole Rorikstead
+#And the braggart did swagger and brandish his blade
+#As he told of bold battles and gold he had made
+#But then he went quiet, did Ragnar the Red
+#When he met the shieldmaiden Matilda who said
+#Oh, you talk and you lie and you drink all our mead
+#Now I think it's high time that you lie down and bleed
+#And so then came clashing and slashing of steel
+#As the brave lass Matilda charged in full of zeal
+#And the braggart named Ragnar was boastful no more
+#When his ugly red head rolled around on the floor
+
